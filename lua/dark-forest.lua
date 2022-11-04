ee9964a3f64fdc0923b7afbf4e4ce168375e39a8
mcoury <mcoury@dwavesys.com>
2022-11-03 18:33:18 -0700

    ...

--- lua/dark-forest.lua
+++ lua/dark-forest.lua
@@ -1,6 +1,6 @@
 -- vim:fdm=marker
 
-local util = require'dark-forest.util'
+local util = require 'dark-forest.util'
 
 local M = {}
 
@@ -10,40 +10,42 @@ local function highlight(group, opts)
   vim.api.nvim_set_hl(0, group, opts)
 end
 
-local bg                = '#112211'
-local fg                = '#adadad'
+local bg = '#112211'
+local fg = '#adadad'
 
-local red               = '#ff0000'
-local green             = '#00ff00'
-local blue              = '#0000ff'
-local yellow            = '#ffff00'
-local magenta           = '#ff00ff'
-local cyan              = '#00ffff'
+local red     = '#ff0000'
+local green   = '#00ff00'
+local blue    = '#0000ff'
+local yellow  = '#ffff00'
+local magenta = '#ff00ff'
+local cyan    = '#00ffff'
 
-local azure             = '#0080ff'
-local chartreuse        = '#80ff00'
-local orange            = '#ff8000'
+local azure      = '#0080ff'
+local chartreuse = '#80ff00'
+local orange     = '#ff8000'
 
-local teal              = '#008080'
-local purple            = '#800080'
-local olive             = '#808000'
+local teal   = '#008080'
+local purple = '#800080'
+local olive  = '#808000'
 
-local white             = '#ffffff'
-local grey_light        = '#cccccc'
-local grey              = '#888888'
-local grey_dark         = '#555555'
-local black             = '#000000'
+local white      = '#ffffff'
+local grey_light = '#cccccc'
+local grey       = '#888888'
+local grey_dark  = '#555555'
+local black      = '#000000'
 
-local yellow_banana     = '#ffe135'
-local green_matrix      = '#03A062'
-local green_dark        = '#003B00'
-local black_vampire     = '#0D0208'
-local green_islamic     = '#008F11'
-local malachite         = '#00ff41'
-local gold              = '#FFD700'
-local gold_dark         = '#AA6C39'
+local yellow_banana = '#ffe135'
+local green_matrix  = '#03A062'
+local green_dark    = '#003B00'
+local black_vampire = '#0D0208'
+local green_islamic = '#008F11'
+local malachite     = '#00ff41'
+local gold          = '#FFD700'
+local gold_dark     = '#AA6C39'
 
 -- }}}
+
+-- Ref: https://neovim.io/doc/user/syntax.html#highlight-groups
 
 -- Editor Highlight Groups {{{
 
@@ -55,8 +57,8 @@ local editor_syntax = {
   DiffAdd      = { fg = green },
   DiffChange   = { fg = yellow },
   DiffDelete   = { fg = red },
-  -- DiffText     = { fg = blue },
-  -- EndOfBuffer  = { bg = bg_dark, fg = bg_dark },
+  DiffText     = { fg = blue },
+  EndOfBuffer  = { fg = green_dark, bg = black },
   ErrorMsg     = { fg = red, bold = true },
   -- VertSplit    = { bg = bg_dark, fg = bg },
   Folded       = { fg = green_matrix, bg = bg, italic = true },
@@ -67,26 +69,31 @@ local editor_syntax = {
   LineNr       = { fg = green_islamic, bg = green_dark, bold = false },
   CursorLineNr = { fg = green_dark, bg = green_islamic, bold = true },
   MatchParen   = { fg = cyan, bold = true },
+  MsgArea      = { bg = bg },
+  -- MsgSeparator  = { fg = fg, bg = bg },
   Normal       = { fg = fg, bg = black_vampire },
   NormalFloat  = { bg = grey_dark, fg = white },
-  -- Pmenu        = { bg = grey_dark, fg = fg_light },
-  -- PmenuSel     = { bg = cyan, fg = bg_lighter, bold = true },
-  -- PmenuSbar    = { bg = bg_lighter },
-  -- PmenuThumb   = { bg = fg },
+  Pmenu        = { bg = grey_dark, fg = grey_light },
+  PmenuSel     = { bg = grey_light, fg = black, bold = true },
+  PmenuSbar    = { bg = bg },
+  PmenuThumb   = { bg = fg },
   -- Search       = { bg = hollow },
   -- SpecialKey   = { bg = bg_light },
   -- SpellBad     = { undercurl = true, sp = red },
   -- SpellCap     = { undercurl = true, sp = yellow },
   -- SpellLocal   = { undercurl = true, sp = orange },
   -- SpellRare    = { undercurl = true, sp = blue },
-  TabLine      = { bg = bg, fg = fg },
-  TabLineFill  = { bg = bg, fg = fg },
-  TabLineSel   = { bg = bg, fg = fg, bold = true},
+  StatusLine   = { fg = fg, bg = green_dark },
+  -- TabLine       = { bg = bg, fg = fg },
+  -- TabLineFill   = { bg = bg, fg = fg },
+  -- TabLineSel    = { bg = bg, fg = fg, bold = true},
   -- Title        = { fg = gold },
   -- Visual       = { bg = hollow_lighter },
   -- VisualNOS    = { bg = hollow_lighter },
-  -- WarningMsg   = { fg = yellow, italic = true },
+  WarningMsg   = { fg = yellow, italic = true },
   -- Whitespace   = { bg = yellow }, -- TODO: i don't know where this is
+  WinBar       = { fg = white, bg = grey },
+  WinBarNC     = { fg = grey_light, bg = grey_dark },
 
   -- lsp
   -- DiagnosticError = { fg = red, bold = true, bg = util.darken(red, 0.1) },
@@ -122,18 +129,6 @@ local editor_syntax = {
   -- gitcommitHeader        = {},
   -- gitcommitFile          = {},
 
-  -- User dependent groups, probably useless to change the default:
-  -- Conceal      = {},
-  -- ModeMsg      = {},
-  -- MsgArea      = {},
-  -- MsgSeparator = {},
-  -- MoreMsg      = {},
-  -- NonText      = {},
-  -- Question     = {},
-  -- QuickFixLine = {},
-  -- StatusLine   = {},
-  -- StatusLineNC = {},
-  -- WildMenu     = {}
 }
 
 -- }}}
@@ -141,32 +136,32 @@ local editor_syntax = {
 -- Vim Default Code Syntax {{{
 
 local code_syntax = {
-  Comment        = { fg = gold_dark, italic = true, bold = true },
-  Constant       = { fg = grey_light, bold = true },
-  String         = { fg = grey_light, bold = true },
-  Character      = { fg = grey_light, bold = true },
-  Number         = { fg = grey_light, bold = true },
-  Float          = { fg = grey_light, bold = true },
-  Boolean        = { fg = grey_light, bold = true },
+  Comment   = { fg = gold_dark, italic = true, bold = true },
+  Constant  = { fg = grey_light, bold = true },
+  String    = { fg = grey_light, bold = true },
+  Character = { fg = grey_light, bold = true },
+  Number    = { fg = grey_light, bold = true },
+  Float     = { fg = grey_light, bold = true },
+  Boolean   = { fg = grey_light, bold = true },
 
-  Identifier     = { fg = fg },
-  Function       = { fg = fg, bold = true },
+  Identifier = { fg = fg },
+  Function   = { fg = fg, bold = true },
 
-  Statement      = { fg = green_matrix, bold = true },
-  Conditional    = { fg = green_matrix, bold = true },
+  Statement   = { fg = green_matrix, bold = true },
+  Conditional = { fg = green_matrix, bold = true },
   -- Repeat         = { fg = blue, italic = true },
   -- Label          = { fg = blue, italic = true },
   -- Exception      = { fg = blue, italic = true },
-  Operator       = { fg = fg, bold = true },
-  Keyword        = { fg = green_matrix, bold = true },
+  Operator    = { fg = fg, bold = true },
+  Keyword     = { fg = green_matrix, bold = true },
 
-  Include        = { fg = yellow_banana },
-  Define         = { fg = yellow_banana },
-  Macro          = { fg = yellow_banana, bold = true },
-  PreProc        = { fg = yellow_banana },
-  PreCondit      = { fg = yellow_banana },
+  Include   = { fg = yellow_banana },
+  Define    = { fg = yellow_banana },
+  Macro     = { fg = yellow_banana, bold = true },
+  PreProc   = { fg = yellow_banana },
+  PreCondit = { fg = yellow_banana },
 
-  Type           = { fg = fg },
+  Type = { fg = fg },
   -- StorageClass   = { fg = yellow },
   -- Structure      = { fg = yellow },
   -- Typedef        = { fg = yellow },
@@ -176,11 +171,11 @@ local code_syntax = {
   -- Tag            = { fg = orange },
   -- SpecialComment = { fg = fg_light, bold = true },
   -- Debug          = {},
-  Delimiter      = { fg = white },
+  Delimiter = { fg = white },
 
   -- Ignore         = {},
   -- Underlined     = { underline = true },
-  Error          = { fg = red },
+  Error = { fg = red },
   -- Todo           = { fg = purple, bold = true },
 }
 
@@ -189,10 +184,10 @@ local code_syntax = {
 -- Plugin Highlight Groups {{{
 
 local plugin_syntax = {
-  GitGutterAdd           = { fg = green },
-  GitGutterChange        = { fg = yellow },
-  GitGutterDelete        = { fg = red },
-  GitGutterChangeDelete  = { fg = orange },
+  GitGutterAdd          = { fg = green },
+  GitGutterChange       = { fg = yellow },
+  GitGutterDelete       = { fg = red },
+  GitGutterChangeDelete = { fg = orange },
 
   -- diffAdded              = { fg = green },
   -- diffRemoved            = { fg = heavy_red },
@@ -320,22 +315,22 @@ function M.setup()
     highlight(group, styles)
   end
 
-  vim.api.nvim_set_var('terminal_color_0',          black_vampire)
-  vim.api.nvim_set_var('terminal_color_1',          red)
-  vim.api.nvim_set_var('terminal_color_2',          green)
-  vim.api.nvim_set_var('terminal_color_3',          yellow)
-  vim.api.nvim_set_var('terminal_color_4',          blue)
-  vim.api.nvim_set_var('terminal_color_5',          purple)
-  vim.api.nvim_set_var('terminal_color_6',          cyan)
-  vim.api.nvim_set_var('terminal_color_7',          fg)
-  vim.api.nvim_set_var('terminal_color_8',          grey)
-  vim.api.nvim_set_var('terminal_color_9',          red)
-  vim.api.nvim_set_var('terminal_color_10',         green)
-  vim.api.nvim_set_var('terminal_color_11',         orange)
-  vim.api.nvim_set_var('terminal_color_12',         blue)
-  vim.api.nvim_set_var('terminal_color_13',         purple)
-  vim.api.nvim_set_var('terminal_color_14',         cyan)
-  vim.api.nvim_set_var('terminal_color_15',         white)
+  vim.api.nvim_set_var('terminal_color_0', black_vampire)
+  vim.api.nvim_set_var('terminal_color_1', red)
+  vim.api.nvim_set_var('terminal_color_2', green)
+  vim.api.nvim_set_var('terminal_color_3', yellow)
+  vim.api.nvim_set_var('terminal_color_4', blue)
+  vim.api.nvim_set_var('terminal_color_5', gold)
+  vim.api.nvim_set_var('terminal_color_6', cyan)
+  vim.api.nvim_set_var('terminal_color_7', fg)
+  vim.api.nvim_set_var('terminal_color_8', grey)
+  vim.api.nvim_set_var('terminal_color_9', red)
+  vim.api.nvim_set_var('terminal_color_10', green)
+  vim.api.nvim_set_var('terminal_color_11', orange)
+  vim.api.nvim_set_var('terminal_color_12', blue)
+  vim.api.nvim_set_var('terminal_color_13', chartreuse)
+  vim.api.nvim_set_var('terminal_color_14', cyan)
+  vim.api.nvim_set_var('terminal_color_15', white)
   vim.api.nvim_set_var('terminal_color_background', black_vampire)
   vim.api.nvim_set_var('terminal_color_foreground', fg)
 end
